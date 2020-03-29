d659b67f84925f9b7b71c8a2d7169033a68b3794
Natallia Vasilyeva <nn1.vasilyeva@gmail.com>
2020-03-29 14:11:11 +0300

    feat: add subtask5

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -1,9 +1,46 @@
 package subtask5
 
+import java.lang.StringBuilder
+
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        var result = StringBuilder(digitString)
+        val stringLength = n;
+        var numberOfRepeats = k;
+        var numberOfNeedChanges = takeNumberOfChangesInString(digitString, stringLength)
+        var differences = numberOfRepeats - numberOfNeedChanges
+
+        if (differences < 0) {
+            return "-1"
+        }
+
+        for (digits in 0 until stringLength / 2) {
+            var startElement = digitString[digits]
+            var endElement = digitString[stringLength - 1 - digits]
+            if (startElement!= endElement && differences>=0) {
+                if (differences > 0) {
+                    result[digits] = '9'
+                    result[stringLength - digits - 1] = '9'
+                    differences--
+                } else {
+                    result[digits] = maxOf(startElement, endElement);
+                    result[stringLength - digits - 1] = maxOf(startElement, endElement);
+                }
+            }
+        }
+    return result.toString()
+}
+
+
+private fun takeNumberOfChangesInString(digitString: String, stringLength: Int): Int {
+    var numberOfChanges = 0
+    for (i in 0 until stringLength / 2) {
+        if (digitString[i] != digitString[digitString.length - i - 1]) {
+            numberOfChanges++
+        }
     }
+    return numberOfChanges;
+}
+
 }
